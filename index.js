d50c334a13e8a284e32d31b954728a77552da575
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-05-19 23:17:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kitten=[];
+function destructivelyAppendKitten(name) {
+  Kitten.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kitten.prepend(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kitten.pop()
+}
+
